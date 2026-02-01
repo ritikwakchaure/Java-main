88265f9c8b07ed48918d7ea6ed07f9f2ec98c6bd
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2026-02-01 16:41:07 +0530

    Added example of == vs equals in String

--- Day-9.1/src/string/Test2.java
+++ Day-9.1/src/string/Test2.java
@@ -1,4 +1,18 @@
 package string;
 
 public class Test2 {
+    //== vs equals
+    public static void main(String[] args) {
+        String s1 = new String( "Testing123");
+        String s2 = new String( "Testing123");
+
+        System.out.println(s1 == s2); //ref equality
+        System.out.println(s1.equals(s2));
+
+         String s3 = new String("Testing123");
+         System.out.println(s1 == s3); //f
+        System.out.println(s1.equals(s3)); //f
+        System.out.println(s1.equalsIgnoreCase(s3)); //t
+
+    }
 }
