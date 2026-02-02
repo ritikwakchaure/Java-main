458eb144d39763d1c6f1f00d677c4067fdf4a003
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2026-02-02 06:38:16 +0530

    Literal String vs Non-Literal String

--- Day-9.1/src/string/Test3.java
+++ Day-9.1/src/string/Test3.java
@@ -1,4 +1,18 @@
 package string;
 
 public class Test3 {
+    public static void main(String[] args) {
+        // literal vs non literal
+        String s1 = "helllo";
+        String s2 = new String(s1);
+        String s3 = "helllo";
+        String s4 = "Helllo";
+        System.out.println(s1 == s2); //f
+        System.out.println(s1.equals(s2));
+        System.out.println(s1 == s3);
+        System.out.println(s1.equals(s3));
+        System.out.println(s1 == s4);
+        System.out.println(s1.equals(s4));
+        System.out.println(s1.equalsIgnoreCase(s4));
+    }
 }
